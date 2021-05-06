3de2b57f7403523b7b1f761e7167e8d2c0006572
Busakuza <kazadan@post.bgu.ac.il>
2021-05-06 19:55:56 +0300

    Maze Generation Server Done

---
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -0,0 +1,46 @@
+package Server;
+
+import IO.SimpleCompressorOutputStream;
+import algorithms.mazeGenerators.*;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class ServerStrategyGenerateMaze implements IServerStrategy{
+    private HashMap<String, IMazeGenerator> mazeGens;
+    public ServerStrategyGenerateMaze(){
+        super();
+        mazeGens = new HashMap<>();
+        mazeGens.put("EmptyMazeGenerator", new EmptyMazeGenerator());
+        mazeGens.put("SimpleMazeGenerator", new SimpleMazeGenerator());
+        mazeGens.put("MyMazeGenerator",  new MyMazeGenerator());
+    }
+    @Override
+    public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
+        try {
+            String tmpFile= "tmp.tmp";
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+            OutputStream toBeCompressedMaze = new SimpleCompressorOutputStream(new FileOutputStream(tmpFile));
+            InputStream in = new FileInputStream(tmpFile);
+
+            int[] dim = (int[])fromClient.readObject();
+             IMazeGenerator mazeGen = mazeGens.get(Configurations.getInstance().getMazeGenerator());
+             Maze maze = mazeGen.generate(dim[0],dim[1]);
+             byte[] compressedMaze = new byte[maze.toByteArray().length];
+             // I have byteArray of the maze, and now we want to send the compressed version to the client
+             toBeCompressedMaze.write(maze.toByteArray());//write it to the tmpFile
+             toBeCompressedMaze.flush();
+             in.read(compressedMaze);//read the compressed maze from the tmpFile
+             toClient.writeObject(compressedMaze);// send to client.
+             toClient.flush();
+             // closing the streams...
+             in.close();
+             fromClient.close();
+             toBeCompressedMaze.close();
+             toClient.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
